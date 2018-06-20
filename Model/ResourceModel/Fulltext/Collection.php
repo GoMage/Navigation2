704468df58ca87fa8c582cc90ed672405a02ed6c
RomanKis <roman.kis.y@gmail.com>
2018-06-20 18:55:20 +0300

    AN2-157: Changes according to review.

--- Model/ResourceModel/Fulltext/Collection.php
+++ Model/ResourceModel/Fulltext/Collection.php
@@ -1,7 +1,8 @@
 <?php
 namespace GoMage\Navigation\Model\ResourceModel\Fulltext;
 
-use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\Api\Search\SearchResultFactory;
 
 class Collection extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection
 {
@@ -9,6 +10,101 @@ class Collection extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Col
      * @var array
      */
     protected $addedFilters = [];
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @param \Magento\Framework\Data\Collection\EntityFactory $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Eav\Model\Config $eavConfig
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Eav\Model\EntityFactory $eavEntityFactory
+     * @param \Magento\Catalog\Model\ResourceModel\Helper $resourceHelper
+     * @param \Magento\Framework\Validator\UniversalFactory $universalFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Module\Manager $moduleManager
+     * @param \Magento\Catalog\Model\Indexer\Product\Flat\State $catalogProductFlatState
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Catalog\Model\Product\OptionFactory $productOptionFactory
+     * @param \Magento\Catalog\Model\ResourceModel\Url $catalogUrl
+     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param \Magento\Customer\Api\GroupManagementInterface $groupManagement
+     * @param \Magento\Search\Model\QueryFactory $catalogSearchData
+     * @param \Magento\Framework\Search\Request\Builder $requestBuilder
+     * @param \Magento\Search\Model\SearchEngine $searchEngine
+     * @param \Magento\Framework\Search\Adapter\Mysql\TemporaryStorageFactory $temporaryStorageFactory
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface|null $connection
+     * @param string $searchRequestName
+     * @param SearchResultFactory|null $searchResultFactory
+     */
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactory $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Eav\Model\EntityFactory $eavEntityFactory,
+        \Magento\Catalog\Model\ResourceModel\Helper $resourceHelper,
+        \Magento\Framework\Validator\UniversalFactory $universalFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Module\Manager $moduleManager,
+        \Magento\Catalog\Model\Indexer\Product\Flat\State $catalogProductFlatState,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Catalog\Model\Product\OptionFactory $productOptionFactory,
+        \Magento\Catalog\Model\ResourceModel\Url $catalogUrl,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        \Magento\Customer\Api\GroupManagementInterface $groupManagement,
+        \Magento\Search\Model\QueryFactory $catalogSearchData,
+        \Magento\Framework\Search\Request\Builder $requestBuilder,
+        \Magento\Search\Model\SearchEngine $searchEngine,
+        \Magento\Framework\Search\Adapter\Mysql\TemporaryStorageFactory $temporaryStorageFactory,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        $searchRequestName = 'catalog_view_container',
+        SearchResultFactory $searchResultFactory = null
+    ) {
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $eavConfig,
+            $resource,
+            $eavEntityFactory,
+            $resourceHelper,
+            $universalFactory,
+            $storeManager,
+            $moduleManager,
+            $catalogProductFlatState,
+            $scopeConfig,
+            $productOptionFactory,
+            $catalogUrl,
+            $localeDate,
+            $customerSession,
+            $dateTime,
+            $groupManagement,
+            $catalogSearchData,
+            $requestBuilder,
+            $searchEngine,
+            $temporaryStorageFactory,
+            $connection,
+            $searchRequestName,
+            $searchResultFactory
+        );
+    }
 
     /**
      * @param string $field
@@ -59,9 +155,7 @@ class Collection extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Col
      */
     public function updateSearchCriteriaBuilder()
     {
-        $searchCriteriaBuilder = ObjectManager::getInstance()
-            ->create(\Magento\Framework\Api\Search\SearchCriteriaBuilder::class);
-        $this->setSearchCriteriaBuilder($searchCriteriaBuilder);
+        $this->setSearchCriteriaBuilder($this->searchCriteriaBuilder);
         return $this;
     }
 
